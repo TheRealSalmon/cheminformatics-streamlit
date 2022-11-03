9e657b416a85c8917448345ce5701d70f0638abd
Sam Mun <therealsam@berkeley.edu>
2022-11-02 23:19:41 -0700

    meep

--- pages/strings_pages/smiles.py
+++ pages/strings_pages/smiles.py
@@ -1,65 +1,65 @@
-import streamlit as st
+# import streamlit as st
 
-import pandas as pd
+# import pandas as pd
 
 # from rdkit_utils import smi_to_svg
 
-def page():
-    st.markdown("""
-        # SMILES
+# def page():
+#     st.markdown("""
+#         # SMILES
 
-        SMILES are a Simplified Molecular Input Line Entry System. In other
-        words, it's a clever way to represent 2D molecules as a 1D
-        string of letters and numbers.
+#         SMILES are a Simplified Molecular Input Line Entry System. In other
+#         words, it's a clever way to represent 2D molecules as a 1D
+#         string of letters and numbers.
 
-        We will start with an overview of the syntax, and then move on to
-        examples of greater and greater complexity.
+#         We will start with an overview of the syntax, and then move on to
+#         examples of greater and greater complexity.
 
-        ## Atoms
+#         ## Atoms
 
-        As with molecules, the most important building block of a SMILES is the
-        atom. Atoms are defined by their atomic symbol.
-    """)
+#         As with molecules, the most important building block of a SMILES is the
+#         atom. Atoms are defined by their atomic symbol.
+#     """)
 
-    smiles = ['C', 'O', '[H]', '[Zn]', 'c1ccccc1']
-    columns = [col for col in st.columns(5)]
-    for smi, col in zip(smiles, columns):
-        col.image(smi_to_svg(smi, img_size=(100, 100)))
-        col.markdown(f'&nbsp;&nbsp;{smi}', unsafe_allow_html=True)
+#     smiles = ['C', 'O', '[H]', '[Zn]', 'c1ccccc1']
+#     columns = [col for col in st.columns(5)]
+#     for smi, col in zip(smiles, columns):
+#         col.image(smi_to_svg(smi, img_size=(100, 100)))
+#         col.markdown(f'&nbsp;&nbsp;{smi}', unsafe_allow_html=True)
 
-    st.markdown("""
-        In general, just the atomic symbol is sufficient. However, for hydrogen
-        and elements with two-letter atomic symbols, brackets `[H]` are
-        required.
+#     st.markdown("""
+#         In general, just the atomic symbol is sufficient. However, for hydrogen
+#         and elements with two-letter atomic symbols, brackets `[H]` are
+#         required.
 
-        Atomic symbols usually have their first letter capitalized. However, the
-        first letter may be lowercase if it is aromatic like this benzene.
+#         Atomic symbols usually have their first letter capitalized. However, the
+#         first letter may be lowercase if it is aromatic like this benzene.
 
-        SMILES also allows us to specify a few atom-level properties such as:
-        * isotope
-        * &#8203;# of hydrogens
-        * formal charge
-    """)
+#         SMILES also allows us to specify a few atom-level properties such as:
+#         * isotope
+#         * &#8203;# of hydrogens
+#         * formal charge
+#     """)
 
-    smiles = ['C', '[13C]', '[13CH4]', '[CH3+]', '[Fe+2]',]
-    columns = [col for col in st.columns(5)]
-    for smi, col in zip(smiles, columns):
-        col.image(smi_to_svg(smi, img_size=(100, 100)))
-        col.markdown(f'&nbsp;&nbsp;{smi}', unsafe_allow_html=True)
+#     smiles = ['C', '[13C]', '[13CH4]', '[CH3+]', '[Fe+2]',]
+#     columns = [col for col in st.columns(5)]
+#     for smi, col in zip(smiles, columns):
+#         col.image(smi_to_svg(smi, img_size=(100, 100)))
+#         col.markdown(f'&nbsp;&nbsp;{smi}', unsafe_allow_html=True)
 
-    st.markdown("""
-        As we see, specifying atom-level properties requires the use of
-        brackets.
-        * The isotope is specified by the numbers that PRECEDE the atomic symbol, ie. `[13C]`.
-        * The # of hydrogens is specified by adding `H` to the end of the atomic symbol and putting the # of hydrogens after the `H`, ie. `[CH3]`.
-        * The formal charge is specified by adding a `+` or `-` after the atom symbol/# of hydrogens. For multiple charges, an additional number can be put after the charge, ie. `[Fe+2]`.
+#     st.markdown("""
+#         As we see, specifying atom-level properties requires the use of
+#         brackets.
+#         * The isotope is specified by the numbers that PRECEDE the atomic symbol, ie. `[13C]`.
+#         * The # of hydrogens is specified by adding `H` to the end of the atomic symbol and putting the # of hydrogens after the `H`, ie. `[CH3]`.
+#         * The formal charge is specified by adding a `+` or `-` after the atom symbol/# of hydrogens. For multiple charges, an additional number can be put after the charge, ie. `[Fe+2]`.
 
-        ### Your turn!
-        """)
+#         ### Your turn!
+#         """)
 
-    smiles = ['N', '[H]', '[He]', '[OH-]', '[18OH2]']
-    columns = [col for col in st.columns(5)]
-    keys = [f'smi{i}' for i in range(5)]
-    for smi, col, key in zip(smiles, columns, keys):
-        col.text_input
-        col.write(smi) 
+#     smiles = ['N', '[H]', '[He]', '[OH-]', '[18OH2]']
+#     columns = [col for col in st.columns(5)]
+#     keys = [f'smi{i}' for i in range(5)]
+#     for smi, col, key in zip(smiles, columns, keys):
+#         col.text_input
+#         col.write(smi) 
--- rdkit_utils.py
+++ rdkit_utils.py
@@ -1,22 +1,22 @@
-from rdkit import Chem
-from rdkit.Chem.Draw.rdMolDraw2D import MolDraw2DSVG, SetDarkMode
+# from rdkit import Chem
+# from rdkit.Chem.Draw.rdMolDraw2D import MolDraw2DSVG, SetDarkMode
 
 
-def mol_to_svg(mol, img_size=(500, 300), show_atom_idx=False):
-    svg_drawer = MolDraw2DSVG(*img_size)
-    SetDarkMode(svg_drawer.drawOptions())
-    if show_atom_idx:
-        svg_drawer.drawOptions().addAtomIndices = True
-    svg_drawer.drawOptions().addStereoAnnotation = True
-    svg_drawer.drawOptions().atomLabelDeuteriumTritium = True
-    svg_drawer.drawOptions().simplifiedStereoGroupLabel = True
-    svg_drawer.drawOptions().fixedBondLength = 20
-    svg_drawer.drawOptions().fixedFontSize = 12
-    svg_drawer.drawOptions().annotationFontScale = 0.6
-    svg_drawer.drawOptions().setBackgroundColour((0.05, 0.05, 0.05))
-    svg_drawer.DrawMolecule(mol)
-    svg_drawer.FinishDrawing()
-    return svg_drawer.GetDrawingText()
+# def mol_to_svg(mol, img_size=(500, 300), show_atom_idx=False):
+#     svg_drawer = MolDraw2DSVG(*img_size)
+#     SetDarkMode(svg_drawer.drawOptions())
+#     if show_atom_idx:
+#         svg_drawer.drawOptions().addAtomIndices = True
+#     svg_drawer.drawOptions().addStereoAnnotation = True
+#     svg_drawer.drawOptions().atomLabelDeuteriumTritium = True
+#     svg_drawer.drawOptions().simplifiedStereoGroupLabel = True
+#     svg_drawer.drawOptions().fixedBondLength = 20
+#     svg_drawer.drawOptions().fixedFontSize = 12
+#     svg_drawer.drawOptions().annotationFontScale = 0.6
+#     svg_drawer.drawOptions().setBackgroundColour((0.05, 0.05, 0.05))
+#     svg_drawer.DrawMolecule(mol)
+#     svg_drawer.FinishDrawing()
+#     return svg_drawer.GetDrawingText()
 
-def smi_to_svg(smi, img_size=(500, 300), show_atom_idx=False):
-    return mol_to_svg(Chem.MolFromSmiles(smi), img_size, show_atom_idx)
+# def smi_to_svg(smi, img_size=(500, 300), show_atom_idx=False):
+#     return mol_to_svg(Chem.MolFromSmiles(smi), img_size, show_atom_idx)
